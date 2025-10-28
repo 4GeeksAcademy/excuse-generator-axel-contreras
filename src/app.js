db4c918627abf1d7e7a42f008423d41315f6c783
axelcontrerasr <ajccr.14@gmail.com>
2025-10-28 15:30:03 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
 
+window.onload = function(GenerarExcusa) {
+  const who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  const action = ['ate', 'peed', 'crushed', 'broke'];
+  const what = ['my homework', 'my phone', 'the car'];
+  const when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+  const sujeto = who[Math.floor(Math.random() * who.length)];
+  const accion = action[Math.floor(Math.random() * action.length)];
+  const objeto = what[Math.floor(Math.random() * what.length)];
+  const lugar = when[Math.floor(Math.random() * when.length)];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  const excusa = `${sujeto} ${accion} ${objeto} ${lugar}.`;
+document.getElementById("excusa").textContent = excusa;
 };
